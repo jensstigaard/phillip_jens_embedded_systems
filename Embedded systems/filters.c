95d54efcaaa74a5bece72ba2bbfec2ea34630e9a
Jens Grønhøj Stigaard <jens@stigaard.info>
2012-09-12 15:00:44 +0200

    .

--- Embedded systems/filters.c
+++ Embedded systems/filters.c
@@ -2,14 +2,12 @@
 #include <stdlib.h>
 #include "filters.h"
 
-int lowPass(int datapoint, int filteredDatapoints[], int rawDatapoints[]);
-int highPass(int datapoint, int filteredDatapoints[], int rawDatapoints[]);
-int derivative(int datapoint, int filteredDatapoints[], int rawDatapoints[]);
-int squaring(int datapoint, int filteredDatapoints[], int rawDatapoints[]);
+void lowPass(int * datapoint, int filteredDatapoints[], int rawDatapoints[]);
+void highPass(int * datapoint, int filteredDatapoints[], int rawDatapoints[]);
+void derivative(int * datapoint, int filteredDatapoints[], int rawDatapoints[]);
+void squaring(int * datapoint);
 
 int runFilters(int datapoint, int filteredDatapoints[], int rawDatapoints[]){
-
-
 
 	lowPass(&datapoint, filteredDatapoints, rawDatapoints);
 	highPass(&datapoint, filteredDatapoints, rawDatapoints);
@@ -22,13 +20,13 @@ int runFilters(int datapoint, int filteredDatapoints[], int rawDatapoints[]){
 
 void lowPass(int* datapoint, int filteredDatapoints[], int rawDatapoints[]){
 
-	datapoint = 2*filteredDatapoints[0] - filteredDatapoints[1] + (datapoint - 2*rawDatapoints[5]+rawDatapoints[11])/32;
+	datapoint = 2 * filteredDatapoints[0] - filteredDatapoints[1] + ((int) datapoint - 2*rawDatapoints[5]+rawDatapoints[11])/32;
 
 }
 
 void highPass(int* datapoint, int filteredDatapoints[], int rawDatapoints[]){
 
-	datapoint = filteredDatapoints[0] - datapoint/32 + rawDatapoints[15] - rawDatapoints[16] + (rawDatapoints[31]/32);
+	datapoint = filteredDatapoints[0] - (int)datapoint/32 + rawDatapoints[15] - rawDatapoints[16] + (rawDatapoints[31]/32);
 
 }
 
@@ -40,6 +38,6 @@ void derivative(int* datapoint, int filteredDatapoints[], int rawDatapoints[]){
 
 void squaring(int* datapoint){
 
-	datapoint = datapoint * datapoint;
+	datapoint = (int)datapoint * (int)datapoint;
 
 }
--- Embedded systems/main.c
+++ Embedded systems/main.c
@@ -6,16 +6,17 @@
 
 static const char filename[] = "ECG.txt";
 
+void shiftArray(int * data_points_raw[], int * data_points_filtered) {
 
-void shiftArray(int * data_points_raw[], int * data_points_filtered){
-
-	for(int i=30; i>=0; i--){
-		data_points_raw[i+1] 		= data_points_raw[i];
-		data_points_filtered[i+1]	= data_points_filtered[i];
+	int i;
+	for (i = 30; i >= 0; i--) {
+		data_points_raw[i + 1] = data_points_raw[i];
+		data_points_filtered[i + 1] = data_points_filtered[i];
 	}
 }
 
-void insertNewDataPoint(int current_point_raw, int current_point_filtered, int * data_points_raw[], int * data_points_filtered){
+void insertNewDataPoint(int current_point_raw, int current_point_filtered,
+		int * data_points_raw[], int * data_points_filtered) {
 	data_points_raw[0] = current_point_raw;
 	data_points_filtered[0] = current_point_filtered;
 }
@@ -24,28 +25,33 @@ int main() {
 
 	int currentPoint = 0;
 	int currentPointFiltered = 0;
-	int filteredDatapoints [32];
-	int rawDatapoints [32];
+	int filteredDatapoints[32];
+	int rawDatapoints[32];
 
-	for(int i = 0; i <32; i++){
+	int i;
+	for (i = 0; i < 32; i++) {
 		filteredDatapoints[i] = 0;
 		rawDatapoints[i] = 0;
 	}
 
 	FILE *file = fopen(filename, "r");
 
-	while(getNextData(file, &currentPoint) != EOF){
+	while (getNextData(file, &currentPoint) != EOF) {
 
 		// Calculate the filtered data
-		currentPointFiltered = filterPoint(currentPoint, rawDatapoints, filteredDatapoints);
+		currentPointFiltered = runFilters(currentPoint, rawDatapoints,
+				filteredDatapoints);
+
+		printf("Current point raw: %i\n", currentPoint);
+		printf("Current point filtered: %i\n", currentPointFiltered);
 
 		// shift data in array 1 field to the right
 		shiftArray(&rawDatapoints, &filteredDatapoints);
 
 		// Inserts the new data point in the first field in both the arrays
-		insertNewDataPoint(currentPoint, currentPointFiltered, &rawDatapoints, &filteredDatapoints);
+		insertNewDataPoint(currentPoint, currentPointFiltered, &rawDatapoints,
+				&filteredDatapoints);
 
-		printf("%i\n", currentPoint);
 	}
 
 	return 0;
